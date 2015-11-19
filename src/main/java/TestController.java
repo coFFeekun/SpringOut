759fbef4db2f41642176db68941f3cce5ea7a0b1
coFFeekun <christophman@hispeed.ch>
2015-11-19 11:12:28 +0100

    initial commit

---
+++ src/main/java/TestController.java
@@ -0,0 +1,36 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Created by Baker on 19.11.2015.
+ */
+
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootApplication
+@EnableAsync
+public class TestController {
+
+    @RequestMapping("/begun")
+    public String map(){
+        return "Duden.de";
+
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        SpringApplication.run(TestController.class, args);
+    }
+}
+
+
+
+
+
---
+++ src/main/java/TestLogic.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Baker on 19.11.2015.
+ */
+public class TestLogic {
+    private boolean logic;
+
+    public boolean isLogic() {
+        return logic;
+    }
+
+    public void setLogic(boolean logic) {
+        this.logic = logic;
+    }
+}
